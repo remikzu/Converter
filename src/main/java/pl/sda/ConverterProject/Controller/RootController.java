9522b125111fde1b3830ae307953e786e6ebda46
Remigiusz Zudzin <remik.zudzin@gmail.com>
2019-03-13 17:55:05 +0100

    creating .gitignore and preparing to javafx module

---
+++ src/main/java/pl/sda/ConverterProject/Controller/RootController.java
@@ -0,0 +1,17 @@
+package pl.sda.ConverterProject.Controller;
+
+import javafx.scene.image.Image;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Remigiusz Zudzin
+ */
+public class RootController {
+
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO change the intiialize method in RootController
+    }
+
+}
---
+++ src/main/java/pl/sda/ConverterProject/Main.java
@@ -0,0 +1,28 @@
+package pl.sda.ConverterProject;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Remigiusz Zudzin
+ */
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    //TODO create sceneBuilder and project WindowApplication
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root =
+                FXMLLoader.load(
+                        getClass()
+                                .getResource(""));
+        primaryStage.setScene(new Scene(root, 657, 400));
+        primaryStage.show();
+    }
+}
